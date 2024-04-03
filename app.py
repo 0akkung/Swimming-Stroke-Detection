e0e4f3eba8458f8cd5b0dcb4e82500f21cad421f
0akkung <pawat.put@ku.th>
2024-04-03 14:54:40 +0700

    Add base.html to be outer layer

--- app.py
+++ app.py
@@ -10,10 +10,28 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 @app.route('/register')
 def register():
     return render_template('register.html')
 
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/swim')
+def swim():
+    return render_template('swim.html')
 
 @app.route('/video_feed')
 def video_feed():
